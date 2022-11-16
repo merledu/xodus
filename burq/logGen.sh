11e060519ddc4a49d86a720d324aca43ce020fdd
5hayanB <baig.sh@proton.me>
2022-11-16 18:17:03 +0500

    added burq isolation

--- burq/logGen.sh
+++ burq/logGen.sh
@@ -4,12 +4,12 @@ python ./scripts/assemblyParser.py --asm ./asm/$1 --hex ./asm/assembly.hex
 # Dump sbt output
 cd ../
 echo "Dumping sbt output..."
-sbt "testOnly Top.TopTest -- -DwriteVcd=1" > ./burq/trace/sbtDump.log
+sbt "testOnly Tracer.TracerTopTest -- -DwriteVcd=1" > ./burq/trace/sbtDump.log
 echo "sbt output successfully dumped"
 
 # Generating log
 cd ./burq
 echo "Generating log..."
-python ./scripts/sbtToLog.py --asm ./asm/$1 --sbt_dump ./trace/sbtDump.log --log ./trace/nrv.log
+python ./scripts/sbtToLog.py --asm ./asm/$1 --sbt_dump ./trace/sbtDump.log --log ./trace/xodus.log
 echo "Log successfully generated"
 
