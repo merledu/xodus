b530f5419af943827f270e461bd292f5ca00f0e3
5hayanB <baig.sh@proton.me>
2022-12-29 18:43:54 +0500

    redesign in progress 10

--- src/main/scala/Core/MemoryStage/DataMemRouter.scala
+++ src/main/scala/Core/MemoryStage/DataMemRouter.scala
@@ -1,4 +1,5 @@
 package MemoryStage
+
 import chisel3._, chisel3.util._
 
 import Memory.{MemReqIO, MemRespIO}
@@ -10,7 +11,7 @@ class DataMemRouterIO(
   enNum    :Int
 ) extends Bundle {
   // Input pins
-  val memVec: SInt = Input(Vec(2, SInt(params("XLEN"))))
+  val data: SInt = Input(Vec(2, SInt(params("XLEN"))))
   val en    : Bool = Input(Vec(enNum, Bool()))
 
   // Output pins
@@ -32,56 +33,66 @@ class DataMemRouter(
   debug       :Boolean
 ) extends Module {
   val enNum: Int = confDataMem.length
-  val io: DataMemRouterIO = IO(new DataMemRouterIO(
+  val io   : DataMemRouterIO = IO(new DataMemRouterIO(
     params   = params,
     paramMap = paramMap,
     enNum    = enNum
   ))
 
   // Wires
-  val uintWires: Map[String, UInt] = Map(
-    "addr" -> io.dataVec(0).asUInt,
-    "sb"   -> io.data
+  val storeWires: Map[String, SInt] = Map(
+    "sb" -> (7, 0),
+    "sh" -> (15, 0)
+  ).map(
+    x._1 -> io.data(1)(x._2._1, x._2._2).asSInt
+  ) ++ Map(
+    "sw" -> io.data(1)
   )
+
+  val loadWires: Map[String, SInt] = Map(
+    "lb" -> (7, 0),
+    "lh" -> (15, 0)
+  ).map(
+    x => x._1 -> io.resp.bits.data(x._2._1, x._2._2)
+  )
+  /*++ Map(
+    "lw" -> io.resp.bits.data
+  )*/
 
   val enWires: Map[String, Bool] = (
     for (i <- 0 until enNum)
-      yield confDataMem(i) -> io.enVec(i)
+      yield confDataMem(i) -> io.en(i)
   ).toMap
 
-  // Store wires
-  val sb      : SInt = dontTouch(WireInit(rs2_data(7, 0).asSInt))
-  val sh      : SInt = dontTouch(WireInit(rs2_data(15, 0).asSInt))
-  val sw      : SInt = dontTouch(WireInit(rs2_data))
-
-  // Storing to data memory
-  io.dataOut := MuxCase(0.S, Seq(
-          sb_en -> sb,
-          sh_en -> sh,
-          sw_en -> sw
-  ))
-  
-  // Load wires
-  val lb : SInt = dontTouch(WireInit(dataIn(7, 0).asSInt))
-  val lh : SInt = dontTouch(WireInit(dataIn(15, 0).asSInt))
-  val lw : SInt = dontTouch(WireInit(dataIn))
-  val lbu: UInt = dontTouch(WireInit(dataIn(7, 0)))
-  val lhu: UInt = dontTouch(WireInit(dataIn(15, 0)))
-
-  // Loading data from data memory
-  io.out := MuxCase(0.S, Seq(
-          lb_en  -> lb,
-          lh_en  -> lh,
-          lw_en  -> lw,
-          lbu_en -> lbu.asSInt,
-          lhu_en -> lhu.asSInt
-  ))
-
+  // Connections
   Seq(
-          io.addrOut, io.loadEn, io.storeEn
-  ) zip Seq(
-          address, load_en, str_en
-  ) foreach {
-          x => x._1 := x._2
-  }
+    (io.out, MuxCase(
+      0.S,
+      for (i <- 1 to 3)
+    ))
+  )
+
+  //// Storing to data memory
+  //io.dataOut := MuxCase(0.S, Seq(
+  //        sb_en -> sb,
+  //        sh_en -> sh,
+  //        sw_en -> sw
+  //))
+  //
+  //// Loading data from data memory
+  //io.out := MuxCase(0.S, Seq(
+  //        lb_en  -> lb,
+  //        lh_en  -> lh,
+  //        lw_en  -> lw,
+  //        lbu_en -> lbu.asSInt,
+  //        lhu_en -> lhu.asSInt
+  //))
+
+  //Seq(
+  //        io.addrOut, io.loadEn, io.storeEn
+  //) zip Seq(
+  //        address, load_en, str_en
+  //) foreach {
+  //        x => x._1 := x._2
+  //}
 }
