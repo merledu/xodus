75c851fbc6548600c31430386db875b95bd2c3a0
5hayanB <baig.sh@proton.me>
2023-01-07 21:48:06 +0500

    redesign in progress 13

--- src/main/scala/core/decode_stage/RegFile.scala
+++ src/main/scala/core/decode_stage/RegFile.scala
@@ -1,56 +1,53 @@
-//package decode_stage
-//
-//import chisel3._, chisel3.util._
-//
-//
-//class RegFileIO(params :Map[String, Int]) extends Bundle {
-//  // Input ports
-//  val rAddr : Vec[UInt]   = Input(Vec(3, UInt(params("regAddrLen").W)))
-//  val rdData: Valid[SInt] = Flipped(Valid(SInt(params("XLEN").W)))
-//
-//  // Output ports
-//  val rsData: Vec[SInt] = Output(Vec(2, SInt(params("XLEN").W)))
-//}
-//
-//
-//class RegFile(
-//  params :Map[String, Int],
-//  debug  :Boolean
-//) extends Module {
-//  val io: RegFileIO = IO(new RegFileIO(params))
-//
-//  // Wires
-//  val addrWires: Map[String, UInt] = Map(
-//    "rdAddr"  -> io.rAddr(0),
-//    "rs1Addr" -> io.rAddr(1),
-//    "rs2Addr" -> io.rAddr(2)
-//  )
-//
-//  // Register File
-//  val regFile: Vec[SInt] = Reg(Vec(params("XLEN"), SInt(params("XLEN").W)))
-//  
-//  // Data is written when valid bit is high
-//  when (io.rdData.valid && addrWires("rdAddr").orR) {
-//    regFile(addrWires("rdAddr") - 1.U) := io.rdData.bits
-//  }
-//
-//  // Connections
-//  for (i <- 0 until io.rsData.length) {
-//    io.rsData(i) := Mux(
-//      addrWires(s"rs${i + 1}Addr").orR,
-//      regFile(addrWires(s"rs${i + 1}Addr")),
-//      0.S
-//    )
-//  }
-//
-//
-//
-//  // Debug Section
-//  if (debug) {
-//    val debug_addrWires_rdAddr : UInt = dontTouch(WireInit(io.rAddr(0)))
-//    val debug_rdData           : SInt = dontTouch(WireInit(io.rdData.bits))
-//    val debug_addrWires_rs1Addr: UInt = dontTouch(WireInit(io.rAddr(1)))
-//    val debug_addrWires_rs2Addr: UInt = dontTouch(WireInit(io.rAddr(2)))
-//    val debug_wrEn             : Bool = dontTouch(WireInit(io.rdData.valid))
-//  } else None
-//}
+package decode_stage
+
+import chisel3._, chisel3.util._
+
+
+class RegFileIO extends Bundle with Configs {
+  // Input ports
+  val rAddr : Vec[UInt]   = Input(Vec(3, UInt(regAddrLen.W)))
+  val rdData: Valid[SInt] = Flipped(Valid(SInt(xlen.W)))
+
+  // Output ports
+  val rsData: Vec[SInt] = Output(Vec(2, SInt(xlen.W)))
+}
+
+
+class RegFile extends Module with Configs {
+  val io: RegFileIO = IO(new RegFileIO)
+
+  // Wires
+  val addrWires: Map[String, UInt] = Map(
+    "rdAddr"  -> io.rAddr(0),
+    "rs1Addr" -> io.rAddr(1),
+    "rs2Addr" -> io.rAddr(2)
+  )
+
+  // Register File
+  val regFile: Vec[SInt] = Reg(Vec(xlen, SInt(xlen.W)))
+  
+  // Data is written when valid bit is high
+  when (io.rdData.valid && addrWires("rdAddr").orR) {
+    regFile(addrWires("rdAddr") - 1.U) := io.rdData.bits
+  }
+
+  // Connections
+  for (i <- 0 until io.rsData.length) {
+    io.rsData(i) := Mux(
+      addrWires(s"rs${i + 1}Addr").orR,
+      regFile(addrWires(s"rs${i + 1}Addr")),
+      0.S
+    )
+  }
+
+
+
+  // Debug Section
+  if (debug) {
+    val debug_addrWires_rdAddr : UInt = dontTouch(WireInit(io.rAddr(0)))
+    val debug_rdData           : SInt = dontTouch(WireInit(io.rdData.bits))
+    val debug_addrWires_rs1Addr: UInt = dontTouch(WireInit(io.rAddr(1)))
+    val debug_addrWires_rs2Addr: UInt = dontTouch(WireInit(io.rAddr(2)))
+    val debug_wrEn             : Bool = dontTouch(WireInit(io.rdData.valid))
+  } else None
+}
